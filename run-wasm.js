d67026d98083c9e13bd83b006c72e74c2e683290
Kassian Wren <the@nodebotani.st>
2022-10-05 09:57:19 +0100

    Code complete JS

---
+++ run-wasm.js
@@ -0,0 +1,23 @@
+'use strict';
+const { readFile } = require('node:fs/promises');
+const { WASI } = require('wasi');
+const { argv, env } = require('node:process');
+const { join } = require('node:path');
+
+const wasi = new WASI({
+  args: argv,
+  env
+});
+
+// Some WASI binaries require:
+//   const importObject = { wasi_unstable: wasi.wasiImport };
+const importObject = { wasi_snapshot_preview1: wasi.wasiImport };
+
+(async () => {
+  const wasm = await WebAssembly.compile(
+    await readFile(join(__dirname, 'wasm-module.wasm'))
+  );
+  const instance = await WebAssembly.instantiate(wasm, importObject);
+
+  wasi.start(instance);
+})();
